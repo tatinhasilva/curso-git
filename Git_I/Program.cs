86f35507277e892ba97550241d631cc83bb514f5
Tatiana Silva <tatinha_silva@hotmail.com>
2021-02-23 15:58:41 -0300

    Projeto iniciado, mostrando a mensagem Olá Mundo

---
+++ Git_I/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Git_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
